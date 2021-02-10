537e30def5fb5884b962553265e566c929cbdfc4
Daniel <danielzaiser91@googlemail.com>
2021-02-10 14:28:44 +0100

    add commonJS

---
+++ bubble-notifications.cjs
@@ -0,0 +1,20 @@
+const BN = class {
+  infoBubbleContainer = null;
+
+  static init() {
+    this.infoBubbleContainer = document.getElementById("infoBubbleContainer");
+  }
+
+  static info(text) {
+    if (!this.infoBubbleContainer) {
+      return console.error("initialize notifications with BN.init() first");
+    }
+    const newBubble = document.createElement("div");
+    newBubble.classList.add("infoBubble", "pop-in");
+    newBubble.innerText = text;
+    this.infoBubbleContainer.appendChild(newBubble);
+    setTimeout(() => this.infoBubbleContainer.removeChild(newBubble), 2500);
+  }
+};
+
+exports = BN;
