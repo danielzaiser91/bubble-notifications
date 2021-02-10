786d7da04d83a22327fa59d542a057efc14dd770
Daniel <danielzaiser91@googlemail.com>
2021-02-10 13:14:09 +0100

    change js to module

--- bubble-notifications.js
+++ bubble-notifications.js
@@ -1,8 +1,19 @@
-const infoBubbleContainer = document.getElementById('infoBubbleContainer');
-function info(text) {
-  const newBubble = document.createElement('div');
-  newBubble.classList.add('infoBubble', 'pop-in');
-  newBubble.innerText = text;
-  infoBubbleContainer.appendChild(newBubble);
-  setTimeout(()=>infoBubbleContainer.removeChild(newBubble),2500);
+const BN = class {
+  infoBubbleContainer = null;
+
+  init() {
+    infoBubbleContainer = document.getElementById('infoBubbleContainer');
+  }
+
+  info(text) {
+    const newBubble = document.createElement('div');
+    newBubble.classList.add('infoBubble', 'pop-in');
+    newBubble.innerText = text;
+    infoBubbleContainer.appendChild(newBubble);
+    setTimeout(() => infoBubbleContainer.removeChild(newBubble), 2500);
+  }
 }
+
+export {
+  BN
+};
